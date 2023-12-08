277d0d2e5bd8995b7729225a272646e7a49190da
Nicolle <nicolle.lim@gmail.com>
2023-12-08 14:16:45 +1300

    duplication
    remove duplication of "for frame in range"

--- BowlingGame.py
+++ BowlingGame.py
@@ -4,3 +4,8 @@ class BowlingGame:
 
     def roll(self,pins):
         self.rolls.append(pins)
+
+    def total_score(self):
+        score = 0
+        roll_index = 0
+        for frame in range(10):
