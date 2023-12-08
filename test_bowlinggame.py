030b4b3ca457cba3e582b31c809e01333919b6f3
Nicolle <nicolle.lim@gmail.com>
2023-12-08 14:59:21 +1300

    assert changed to self.assetEqual
    assert changed to self.assetEqual

--- test_bowlinggame.py
+++ test_bowlinggame.py
@@ -9,3 +9,7 @@ class TestBowlingGame(unittest.TestCase):
     def test_gutter_game(self):
         self._roll_many(0, 20)
         self.assertEqual(0, self.game.total_score())
+
+    def test_all_ones(self):
+        self._roll_many(1, 20)
+        self.assertEqual(20, self.game.total_score())
