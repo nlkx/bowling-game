03478b7e79719c1110956be7d7feebcee03395b7
Nicolle <nicolle.lim@gmail.com>
2023-12-08 14:57:08 +1300

    naming method
    changed naming method and instead of "assert.." changed to "self.assertEqual"

--- test_bowlinggame.py
+++ test_bowlinggame.py
@@ -5,3 +5,7 @@ class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
         self.game = BowlingGame.BowlingGame()
+
+    def test_gutter_game(self):
+        self._roll_many(0, 20)
+        self.assertEqual(0, self.game.total_score())
