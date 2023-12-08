fa143f049d0d5255ff66f6945ba939a60747a1bb
Nicolle <nicolle.lim@gmail.com>
2023-12-08 16:26:13 +1300

    Update test_bowlinggame.py

--- test_bowlinggame.py
+++ test_bowlinggame.py
@@ -6,36 +6,36 @@ class TestBowlingGame(unittest.TestCase):
     def setUp(self):
         self.game = BowlingGame.BowlingGame()
 
-    def test_gutter_game(self):
+    def test_gutter_game(self): #changed naming method
         self._roll_many(0, 20)
-        self.assertEqual(0, self.game.total_score())
+        self.assertEqual(0, self.game.total_score()) #change assert to self.assertEqual
 
-    def test_all_ones(self):
+    def test_all_ones(self): #changed naming method
         self._roll_many(1, 20)
-        self.assertEqual(20, self.game.total_score())
+        self.assertEqual(20, self.game.total_score()) #change assert to self.assertEqual
 
-    def test_one_spare(self):
+    def test_one_spare(self): #changed naming method
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
         self._roll_many(0, 17)
-        self.assertEqual(16, self.game.total_score())
+        self.assertEqual(16, self.game.total_score()) #change assert to self.assertEqual
 
-    def test_one_strike(self):
+    def test_one_strike(self): #changed naming method
         self.game.roll(10)
         self.game.roll(3)
         self.game.roll(4)
         self._roll_many(0, 16)
-        self.assertEqual(24, self.game.total_score())
+        self.assertEqual(24, self.game.total_score()) #change assert to self.assertEqual
 
-    def test_perfect_game(self):
+    def test_perfect_game(self): #changed naming method
         self._roll_many(10, 12)
-        self.assertEqual(300, self.game.total_score())
+        self.assertEqual(300, self.game.total_score()) #change assert to self.assertEqual
 
-    def test_one_spare(self):
+    def test_one_spare(self): #changed naming method
         self._roll_many(5, 21)
-        self.assertEqual(150, self.game.total_score())
+        self.assertEqual(150, self.game.total_score()) #change assert to self.assertEqual
 
-    def _roll_many(self, pins, num):
+    def _roll_many(self, pins, num): #changed naming method
         for i in range(num):
-            self.game.roll(pins)
+            self.game.roll(pins) # changed rolls to num 
