e4d9f72241f1e452705ac0dba18ef5640cf697c9
Nicolle <nicolle.lim@gmail.com>
2023-12-08 15:20:58 +1300

    naming changed
    testOneSpare changed to test_one_spare

--- test_bowlinggame.py
+++ test_bowlinggame.py
@@ -13,3 +13,12 @@ class TestBowlingGame(unittest.TestCase):
     def test_all_ones(self):
         self._roll_many(1, 20)
         self.assertEqual(20, self.game.total_score())
+
+    def test_one_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(3)
+        self._roll_many(0, 17)
+        self.assertEqual(16, self.game.total_score())
+
+    
